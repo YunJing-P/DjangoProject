a9b76421618a40ec92550923d733fd5e7c9c0b0d
pyj <15975343813@163.com>
2021-02-02 13:40:58 +0800

    fix bug

--- mysite/feishu/urls.py
+++ mysite/feishu/urls.py
@@ -6,6 +6,5 @@ from django.views.decorators.csrf import csrf_exempt
 app_name = 'feishu'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('token/', Api.update_token),
     path('api/', csrf_exempt(Api.as_view())),
 ]
